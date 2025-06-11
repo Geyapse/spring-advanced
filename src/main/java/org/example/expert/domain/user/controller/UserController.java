edad5ebb6985c2b449f7bfbf16eee44b103771e8
Geyapse <8290m@naver.com>
2025-06-11 09:57:41 +0900

    레벨 1 - 3 코드 개선 퀴즈 - Validation 서비스 메서드에서 하던 비밀번호 조건검사를 DTO에서 어노테이션으로 조건검사실행 검증로직 재사용 가능 유지보수 용이

--- src/main/java/org/example/expert/domain/user/controller/UserController.java
+++ src/main/java/org/example/expert/domain/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package org.example.expert.domain.user.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.expert.domain.common.annotation.Auth;
 import org.example.expert.domain.common.dto.AuthUser;
@@ -7,6 +8,8 @@ import org.example.expert.domain.user.dto.request.UserChangePasswordRequest;
 import org.example.expert.domain.user.dto.response.UserResponse;
 import org.example.expert.domain.user.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,7 +24,16 @@ public class UserController {
     }
 
     @PutMapping("/users")
-    public void changePassword(@Auth AuthUser authUser, @RequestBody UserChangePasswordRequest userChangePasswordRequest) {
+    public void changePassword(
+            @Auth AuthUser authUser,
+            @Valid @RequestBody UserChangePasswordRequest userChangePasswordRequest,
+            BindingResult bindingResult
+    ) throws MethodArgumentNotValidException {
+        if (bindingResult.hasErrors()) {
+            throw new MethodArgumentNotValidException(null, bindingResult);
+        }
+
         userService.changePassword(authUser.getId(), userChangePasswordRequest);
+
     }
 }
